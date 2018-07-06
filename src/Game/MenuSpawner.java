fcd3202cc3bf7dc201a998e9ad9549c482e4729c
Collerbasti <webdeseb1@gmail.com>
2018-07-06 15:08:58 +0200

    Bugfix

--- src/Game/MenuSpawner.java
+++ src/Game/MenuSpawner.java
@@ -27,6 +27,7 @@ public class MenuSpawner {
 			Count = Count -1;
 			
 			
+			
 			if(Count == 4 || Count == 3 || Count == 2 || Count == 1 || Count == 0) {
 				int p1Mobzone = Count +1;
 				if(main.cfg.getBoolean(Arena+".p1"+".mobfield."+p1Mobzone+".isinuse")) {
@@ -34,7 +35,7 @@ public class MenuSpawner {
 						
 						ItemStack Skull = new ItemStack(Material.WOOD , 1);
 				    	ItemMeta IMeta = (ItemMeta) Skull.getItemMeta(); 
-				    	IMeta.setDisplayName(main.cfg.getString("Verdekte Karte"));
+				    	IMeta.setDisplayName("Verdekte Karte");
 				    	Skull.setItemMeta(IMeta);
 				    	myInventory.setItem(Count,Skull);
 						
@@ -46,36 +47,31 @@ public class MenuSpawner {
 			    	Skull.setDurability((short) main.cfg.getInt(Arena+".p1"+".mobfield."+p1Mobzone+".mobint"));
 			    	myInventory.setItem(Count,Skull);
 				}
+				}
 					
-					
-			}
+			}else if(Count == 13 || Count == 12 || Count == 11 || Count == 10 || Count == 9) {
 				
-				
-				if(Count == 13 || Count == 12 || Count == 11 || Count == 10 || Count == 9) {
 					int p1wizzone = Count -8;
-					if(main.cfg.getBoolean(Arena+".p1"+".Wizzone."+p1wizzone+".isinuse")) {
+					if(main.cfg.getBoolean(Arena+".p1"+".wizzone."+p1wizzone+".isinuse")) {
 						
 						if(main.cfg.getString(Arena+".p1"+".mobfield."+p1wizzone+".mobname")=="Verdekte Karte") {
 							
 							ItemStack Skull = new ItemStack(Material.WOOD , 1);
 					    	ItemMeta IMeta =  Skull.getItemMeta(); 
-					    	IMeta.setDisplayName(main.cfg.getString("Verdekte Karte"));
+					    	IMeta.setDisplayName("Verdekte Karte");
 					    	Skull.setItemMeta(IMeta);
 					    	myInventory.setItem(Count,Skull);
 							
 						}
-				}
-					
-					
-					
-					if(Count == 22 || Count == 21 || Count == 20 || Count == 19 || Count == 18) {
+					}
+				}else if(Count == 22 || Count == 21 || Count == 20 || Count == 19 || Count == 18) {
 						int p2mobzone = Count -17;
 						if(main.cfg.getBoolean(Arena+".p2"+".mobfield."+p2mobzone+".isinuse")) {
 							if(main.cfg.getString(Arena+".p2"+".mobfield."+p2mobzone+".mobname")=="Verdekte Karte") {
 								
 								ItemStack Skull = new ItemStack(Material.WOOD , 1);
 						    	ItemMeta IMeta =  Skull.getItemMeta(); 
-						    	IMeta.setDisplayName(main.cfg.getString("Verdekte Karte"));
+						    	IMeta.setDisplayName("Verdekte Karte");
 						    	Skull.setItemMeta(IMeta);
 						    	myInventory.setItem(Count,Skull);
 						    	
@@ -87,33 +83,33 @@ public class MenuSpawner {
 						    	Skull.setDurability((short) main.cfg.getInt(Arena+".p2"+".mobfield."+p2mobzone+".mobint"));
 						    	myInventory.setItem(Count,Skull);
 							}
-					}
+						}
+					}else if(Count == 31 || Count == 30 || Count == 29 || Count == 28 || Count == 27) {
 						
-						
-						if(Count == 31 || Count == 30 || Count == 29 || Count == 28 || Count == 27) {
+						p.sendMessage(String.valueOf(Count));
 							int p2wizzone = Count -26;
-							if(main.cfg.getBoolean(Arena+".p2"+".Wizzone."+p2wizzone+".isinuse")) {
+							if(main.cfg.getBoolean(Arena+".p2"+".wizzone."+p2wizzone+".isinuse")) {
 								
 								if(main.cfg.getString(Arena+".p2"+".Wizzone."+p2wizzone+".mobname")=="Verdekte Karte") {
 									
 									ItemStack Skull = new ItemStack(Material.WOOD , 1);
 							    	ItemMeta IMeta = Skull.getItemMeta(); 
-							    	IMeta.setDisplayName(main.cfg.getString("Verdekte Karte"));
+							    	IMeta.setDisplayName("Verdekte Karte");
 							    	Skull.setItemMeta(IMeta);
 							    	myInventory.setItem(Count,Skull);
 								}
 								
 						}					
 		}
-	}
+	
 	
 	
 	
 
 	
 	
-}
-	}
+
+	
 			}
 	p.openInventory(myInventory);	
 	}
