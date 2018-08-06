b5df501878878c47ba23ba722a6b4a035fe0ef0f
Collerbasti <webdeseb1@gmail.com>
2018-08-06 08:35:54 +0200

    Hallo

--- src/Wizzcards/Wizzcards_Main.java
+++ src/Wizzcards/Wizzcards_Main.java
@@ -1,5 +1,22 @@
 package Wizzcards;
 
-public class Wizzcards_Main {
+import java.util.ArrayList;
 
+public class Wizzcards_Main {
+	
+	
+	
+	
+	
+	
+	public static ArrayList<String> listOfAllCards() {
+		ArrayList<String> WizzCards = new ArrayList<String>();
+		
+		WizzCards.add("Sword Of Destiny");
+		WizzCards.add("Trank der Heilung");
+
+		return WizzCards;
+	}
+	
+	
 }
